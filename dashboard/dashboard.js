499a7a3054d471a860bc8cd80ad09c45371bad46
Justin2997 <justin.brlotte797@gmail.com>
2019-07-29 09:40:36 -0400

    Fix Sheets

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -25,8 +25,8 @@ function handleClientLoad() {
  */
 function initClient() {
     console.log("initClient")
-    const API_KEY = "AIzaSyAfuXvF2zSTnot4Km8vD0ok4TM59AeIdm0";
-    const CLIENT_ID = "985468569936-oammu0vpn5mrum9ceiraq34q4vn1jcnb.apps.googleusercontent.com"; // This is not a commit file
+    const API_KEY = "AIzaSyC9vcibuupD8VTkcDLC7mwYTLrWcRwqol4";
+    const CLIENT_ID = "579995592859-3ogboqk5n4gc938c64f3qfeip2mbnlgi.apps.googleusercontent.com"; // This is not a commit file
     const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
     const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
     
@@ -51,6 +51,42 @@ function initClient() {
         signoutButton.onclick = handleSignoutClick;
     }, function(error) {
         appendPre(JSON.stringify(error, null, 2));
+    });
+}
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+function listMajors() {
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+        range: 'Class Data!A2:E',
+    }).then(function(response) {
+        var range = response.result;
+        if (range.values.length > 0) {
+        appendPre('Name, Major:');
+        for (i = 0; i < range.values.length; i++) {
+            var row = range.values[i];
+            // Print columns A and E, which correspond to indices 0 and 4.
+            appendPre(row[0] + ', ' + row[4]);
+        }
+        } else {
+        appendPre('No data found.');
+        }
+    }, function(response) {
+        appendPre('Error: ' + response.result.error.message);
     });
 }
 
