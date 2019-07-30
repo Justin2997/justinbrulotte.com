5caa0ea25c3571c624265c1caa72ac97de4b7f1d
Justin2997 <justin.brlotte797@gmail.com>
2019-07-30 11:16:18 -0400

    Update ready for Dashboard and Medium

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -7,15 +7,13 @@ $(window).on('load', function() {
     getDashboardInfo(key, token, listName);
 })
 
-async function getFitBitInformation(key, client_id, scopes){
-
-}
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
 
 /**
  *  On load, called to load the auth2 library and API client library.
  */
 function handleClientLoad() {
-    console.log("handleClientLoad")
     gapi.load('client:auth2', initClient);
 }
 
@@ -24,14 +22,10 @@ function handleClientLoad() {
  *  listeners.
  */
 function initClient() {
-    console.log("initClient")
     const API_KEY = "AIzaSyC9vcibuupD8VTkcDLC7mwYTLrWcRwqol4";
     const CLIENT_ID = "579995592859-3ogboqk5n4gc938c64f3qfeip2mbnlgi.apps.googleusercontent.com"; // This is not a commit file
     const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
     const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
-    
-    var authorizeButton = document.getElementById('authorize_button');
-    var signoutButton = document.getElementById('signout_button');
     
     gapi.client.init({
         apiKey: API_KEY,
@@ -39,11 +33,8 @@ function initClient() {
         discoveryDocs: DISCOVERY_DOCS,
         scope: SCOPES
     }).then(function () {
-        console.log("done")
         // Listen for sign-in state changes.
         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-        console.log("getAuthInstance")
 
         // Handle the initial sign-in state.
         updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
@@ -53,37 +44,36 @@ function initClient() {
         appendPre(JSON.stringify(error, null, 2));
     });
 }
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
+
 function appendPre(message) {
-    var pre = document.getElementById('content');
+    var pre = document.getElementById('activity_content');
     var textContent = document.createTextNode(message + '\n');
     pre.appendChild(textContent);
 }
 
-/**
- * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- */
-function listMajors() {
+function listActivity() {
     gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-        range: 'Class Data!A2:E',
+        spreadsheetId: '16dVoDQw56K-6d522SKjp8I31zyM9-kRK5CIkhcUDngc',
+        range: 'Data!A:L',
     }).then(function(response) {
         var range = response.result;
         if (range.values.length > 0) {
-        appendPre('Name, Major:');
-        for (i = 0; i < range.values.length; i++) {
-            var row = range.values[i];
-            // Print columns A and E, which correspond to indices 0 and 4.
-            appendPre(row[0] + ', ' + row[4]);
-        }
-        } else {
-        appendPre('No data found.');
+            appendPre('Date , Time');
+            var average = 0;
+            for (i = 0; i < range.values.length; i++) {
+                var row = range.values[i];
+                average = average + parseInt(row[11]);
+                if (getWeek(new Date()) === getWeek(new Date(row[0])) && parseInt(row[11]) != 0){
+                    appendPre(row[0] + ', ' + row[11] + ' minutes');
+                }
+            }
+            average = average / range.values.length;
+            var text = "<h5> Average activity : " + parseInt(average) + " minutes </h5>";
+            text +=  "<h5> Total number of activity : " + range.values.length + "</h5>"
+            $('#average_activity').html(text);
+        } 
+        else {
+            appendPre('No data.');
         }
     }, function(response) {
         appendPre('Error: ' + response.result.error.message);
@@ -112,12 +102,37 @@ function updateSigninStatus(isSignedIn) {
     if (isSignedIn) {
         authorizeButton.style.display = 'none';
         signoutButton.style.display = 'block';
-        listMajors();
+        listActivity();
     } else {
         authorizeButton.style.display = 'block';
         signoutButton.style.display = 'none';
     }
 }
+
+var getWeek = function (d) {
+    // Create a copy of this date object  
+    var target  = new Date(d.valueOf());  
+    
+    // ISO week date weeks start on monday  
+    // so correct the day number  
+    var dayNr   = (d.getDay() + 6) % 7;  
+  
+    // Set the target to the thursday of this week so the  
+    // target date is in the right year  
+    target.setDate(target.getDate() - dayNr + 3);  
+  
+    // ISO 8601 states that week 1 is the week  
+    // with january 4th in it  
+    var jan4    = new Date(target.getFullYear(), 0, 4);  
+  
+    // Number of days between target date and january 4th  
+    var dayDiff = (target - jan4) / 86400000;    
+  
+    // Calculate week number: Week 1 (january 4th) plus the    
+    // number of weeks between target date and january 4th    
+    var weekNr = 1 + Math.ceil(dayDiff / 7);    
+    return weekNr;    
+  };
 
 var sort_by = function(field, reverse, primer){
     var key = function (x) {return primer ? primer(x[field]) : x[field];};
