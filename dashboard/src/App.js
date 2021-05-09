325f7d08af49a76cb87a72bf5b091336d142e664
Justin Brûlotte <justin.brlotte797@gmail.com>
2021-05-08 23:51:55 -0400

    Adding configuration for Auth0
    Signed-off-by: Justin Brûlotte <justin.brlotte797@gmail.com>

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,7 +1,8 @@
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/core';
+import { ThemeProvider, CircularProgress } from '@material-ui/core';
+import { useAuth0 } from '@auth0/auth0-react';
 import GlobalStyles from 'src/components/GlobalStyles';
 import 'src/mixins/chartjs';
 import theme from 'src/theme';
@@ -9,6 +10,12 @@ import routes from 'src/routes';
 
 const App = () => {
   const routing = useRoutes(routes);
+  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+
+  if (!isAuthenticated || !isLoading) {
+    loginWithRedirect();
+    return (<CircularProgress>Loading ...</CircularProgress>);
+  }
 
   return (
     <ThemeProvider theme={theme}>
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render((
   <BrowserRouter>
-    <App />
+    <Auth0Provider
+      domain="dev-pomwe-m8.us.auth0.com"
+      clientId="miljlZt93f1F30WDEvyWoWXlJ29vy30F"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </BrowserRouter>
 ), document.getElementById('root'));
 
